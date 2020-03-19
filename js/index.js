92cebaaf2efe5d712e4b52d3e271517395308309
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2020-03-19 18:22:42 +0100

    updaterad

--- js/index.js
+++ js/index.js
@@ -1,42 +1,23 @@
 /*-------------------------Variables-------------------------*/
 
 //URL to Flickr with API-Key
-const url = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1&method=flickr.photos.search`;
+const basicUrl = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&method=flickr.photos.search`;
 
 //URL searching via input fields
-const urlSearchInputText = `&text${inputText}`;
+const urlSearchInputText = `&text=${inputText}`;
 
 //URL searching via format and how many images
-const urlSearchFormat = `&per_page${numberOfImages}&page=1&format=json`;
+const urlSearchFormat = `&per_page=${numberOfImages}&page=1&format=json`;
 
 //The searchbutton
 const searchButton = document.getElementById('search');
 
 //The Images
-let images = document.getElementById('images');
+let imagesElem = document.getElementById('images');
 
 //Module to function getImages
 //import {getImages} from "modules/getImages";
 
-
-
-/*-------------------------Click on button-------------------------*/
-searchButton.addEventListener('click', function() {
-    let inputText = document.getElementById('inputText').value;
-    //let numberOfImages = document.getElementById('numberOfImages').value;
-    getImages(inputText);
-});
-
-
-/*-------------------------Get images-------------------------*/
-function getImages(inputText) {
-    let url = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1&method=flickr.photos.search&text${inputText}`;
-    fetch(url, {method:'GET'} ).then ( (response) =>  { 
-        return response.json();	
-	}).then( (data) => {
-		console.log(data);
-	}).catch( (error => console.log('ERROR IN FETCH: ', error)));
-}
 
 /*-------------------------Show images-------------------------*/
 function showImages(images) {
@@ -47,8 +28,30 @@ function showImages(images) {
 
 function loopImages(images) {
     imagesElem.innerHTML = '';
-
     for(let i = 0; i < images.length; i++) {
         showImages(images[i]);
     }
 }
+
+/*-------------------------Get images-------------------------*/
+async function getImages(inputText, numberOfImages) {
+    let searchUrl = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&method=flickr.photos.search&text=${inputText}&per_page=${numberOfImages}&page=1&format=json`;
+    let url = searchUrl;
+    
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        loopImages(data);
+        console.log(response.json());
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+
+/*-------------------------Click on button-------------------------*/
+searchButton.addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    let numberOfImages = document.getElementById('numberOfImages').value;
+    getImages(inputText, numberOfImages);
+});
