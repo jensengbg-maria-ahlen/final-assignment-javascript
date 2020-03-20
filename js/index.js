e5b0e538fa15ebd015ea386aacc5c110c66a0c78
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2020-03-20 13:54:54 +0100

    ny

--- js/index.js
+++ js/index.js
@@ -1,51 +1,47 @@
 /*-------------------------Variables-------------------------*/
-
-//URL to Flickr with API-Key
-const basicUrl = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&method=flickr.photos.search`;
-
-//URL searching via input fields
-const urlSearchInputText = `&text=${inputText}`;
-
-//URL searching via format and how many images
-const urlSearchFormat = `&per_page=${numberOfImages}&page=1&format=json`;
-
 //The searchbutton
 const searchButton = document.getElementById('search');
 
 //The Images
 let imagesElem = document.getElementById('images');
 
-//Module to function getImages
-//import {getImages} from "modules/getImages";
 
-
-/*-------------------------Show images-------------------------*/
+/*-------------------------Add images to HTML-------------------------*/
 function showImages(images) {
     let elem = document.createElement('div');
     elem.innerHTML = images;
     imagesElem.append(elem);
 }
 
-function loopImages(images) {
+function addImages(images) {
     imagesElem.innerHTML = '';
-    for(let i = 0; i < images.length; i++) {
-        showImages(images[i]);
+    for(let i = 0; i < images.photos.photo.length; i++) {
+        showImages(images.photos.photo[i]);
+        getInfo(images.photos.photo[i]);
     }
 }
 
+
 /*-------------------------Get images-------------------------*/
 async function getImages(inputText, numberOfImages) {
-    let searchUrl = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&method=flickr.photos.search&text=${inputText}&per_page=${numberOfImages}&page=1&format=json`;
+    let searchUrl = `https://api.flickr.com/services/rest?&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d&method=flickr.photos.search&text=${inputText}&per_page=${numberOfImages}&page=1&format=json&nojsoncallback=1`;
     let url = searchUrl;
     
     try {
         let response = await fetch(url);
         let data = await response.json();
-        loopImages(data);
-        console.log(response.json());
+        addImages(data);
     } catch(error) {
         console.error(error);
     }
+}
+
+
+/*-------------------------Get info in images-------------------------*/
+function getInfo(images) {
+    let infoURL = `https://farm${images.farm}.staticflickr.com/${images.server}/${images.id}_${images.secret}.jpg`;
+    console.log(images);
+    fetch(infoURL);
 }
 
 
